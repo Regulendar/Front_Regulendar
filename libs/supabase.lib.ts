127cf1c2584760e6617dcb5821e1d0143653d707
MILGAM <93873127+Milgam06@users.noreply.github.com>
2025-09-02 18:23:57 +0900

    feat: add initialize setting props in supabase client

--- libs/supabase.lib.ts
+++ libs/supabase.lib.ts
@@ -1,5 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { createClient } from '@supabase/supabase-js';
+import { createClient, processLock } from '@supabase/supabase-js';
 import { Platform } from 'react-native';
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
@@ -15,5 +15,9 @@ console.log('Supabase Public Key:', supabasePublicKey);
 export const supabase = createClient(supabaseUrl, supabasePublicKey, {
   auth: {
     ...(Platform.OS !== 'web' ? { storage: AsyncStorage } : {}),
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+    lock: processLock,
   },
 });
