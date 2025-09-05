ea591ea5eabc7f40c17a229519133f5e59b89a35
Milgam06 <milgamfruit@gmail.com>
2025-09-06 01:56:04 +0900

    feat: add Email&Password Input in SignInScreen

--- screens/register/signIn/signIn.tsx
+++ screens/register/signIn/signIn.tsx
@@ -1,12 +1,83 @@
-import { memo } from 'react';
+import { memo, useCallback, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Stack, Text } from 'tamagui';
+import { isEmail, isMobilePhone } from 'validator';
 
+import { Input } from '@/components';
+import { supabaseAuth } from '@/libs';
+
+// TODO(@Milgam06): Button 추가 필요
 export const SignInScreen = memo(() => {
+  const [emailOrNumber, setEmailOrNumber] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleChangeEmailOrNumber = useCallback((text: string) => {
+    setEmailOrNumber(text);
+  }, []);
+
+  const handleChangePassword = useCallback((text: string) => {
+    setPassword(text);
+  }, []);
+
+  const handlePressSignIn = useCallback(() => {
+    const hasEmailOrPhone = emailOrNumber.length > 0;
+    const hasPassword = password.length > 0;
+    const isInputValid = hasEmailOrPhone && hasPassword;
+    if (!isInputValid) {
+      return;
+    }
+    const isValidEmail = isEmail(emailOrNumber);
+    const isValidPhoneNumber = isMobilePhone(emailOrNumber, 'ko-KR');
+
+    if (isValidEmail) {
+      supabaseAuth.signInWithPassword({ email: emailOrNumber, password });
+      return;
+    }
+    if (isValidPhoneNumber) {
+      supabaseAuth.signInWithPassword({ phone: emailOrNumber, password });
+      return;
+    }
+  }, [emailOrNumber, password]);
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <Stack>
-        <Text>Sign In Screen</Text>
+      <Stack flex={1} width="$fluid">
+        <Stack items="center" px="$size.x5" py="$size.x10" gap="$size.x8">
+          <Stack width="$fluid" items="center" gap="$size.x2">
+            <Text fontSize="$9" fontWeight="800" color="$colors.black">
+              돌아오신 것을 환영해요!
+            </Text>
+            <Text fontSize="$6" fontWeight="700" color="$colors.mediumGray">
+              사용자님의 계정으로 로그인해주세요.
+            </Text>
+          </Stack>
+          <Stack width="$fluid" gap="$size.x5">
+            <Input
+              w="$fluid"
+              placeholder="example@example.com"
+              labelContent="이메일 or 전화번호"
+              keyboardType="email-address"
+              size="$x12"
+              px="$size.x3"
+              borderColor="$colors.lightGray"
+              fontWeight="500"
+              focusStyle={{ borderColor: '$colors.darkGreen', borderWidth: 2 }}
+              onChangeText={handleChangeEmailOrNumber}
+            />
+            <Input
+              w="$fluid"
+              placeholder="password"
+              labelContent="비밀번호"
+              secureTextEntry
+              size="$x12"
+              px="$size.x3"
+              borderColor="$colors.lightGray"
+              fontWeight="500"
+              focusStyle={{ borderColor: '$colors.darkGreen', borderWidth: 2 }}
+              onChangeText={handleChangePassword}
+            />
+          </Stack>
+        </Stack>
       </Stack>
     </SafeAreaView>
   );
