8aa1be0e1e71b569a30a113a72ca0033dd04a5a3
MILGAM <93873127+Milgam06@users.noreply.github.com>
2025-09-06 09:47:31 +0900

    feat: route RegisterScreen depends on UserData

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,5 @@
 import { Redirect } from 'expo-router';
 
 export default function () {
-  // return <Redirect href="/greeting/greeting" />;
-  return <Redirect href="/register/register" />;
+  return <Redirect href="/greeting/greeting" />;
 }
