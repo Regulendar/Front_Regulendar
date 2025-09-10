5153e0de06ec9a9c91ae6960b1e45c2f5a351dd4
Milgam06 <milgamfruit@gmail.com>
2025-09-10 19:21:04 +0900

    feat: add CalendarComponent in HomeScreen

--- screens/home/home.tsx
+++ screens/home/home.tsx
@@ -1,11 +1,14 @@
+import { Calendar } from '@/components';
 import { memo } from 'react';
+
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Stack, Text } from 'tamagui';
 
 export const HomeScreen = memo(() => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Stack flex={1} justify="center" items="center">
+    <SafeAreaView edges={['top']} style={{ flex: 1 }}>
+      <Stack flex={1} width="$fluid" borderWidth={1} justify="space-between" items="center">
+        <Calendar />
         <Text>HomeScreen</Text>
       </Stack>
     </SafeAreaView>
