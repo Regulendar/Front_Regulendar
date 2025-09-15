448381d417c93760eea42f2216b3f2c94ee7aea4
Milgam06 <milgamfruit@gmail.com>
2025-09-15 23:21:36 +0900

    test: vercel deploy test

--- api/graphql.ts
+++ api/graphql.ts
@@ -1,12 +1,16 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { createApp } from '../src/main';
+// import type { VercelRequest, VercelResponse } from '@vercel/node';
+// import { createApp } from '../src/main';
 
-const cache: { handler?: any } = {};
+// const cache: { handler?: any } = {};
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  if (!cache.handler) {
-    const app = await createApp();
-    cache.handler = app.getHttpAdapter().getInstance();
-  }
-  return cache.handler(req, res);
+// export default async function handler(req: VercelRequest, res: VercelResponse) {
+//   if (!cache.handler) {
+//     const app = await createApp();
+//     cache.handler = app.getHttpAdapter().getInstance();
+//   }
+//   return cache.handler(req, res);
+// }
+
+export default function handler(req, res) {
+  return res.status(200).json({ ok: true });
 }
