d3fbb62863761bffa764099f4f509b49c6178778
Milgam06 <milgamfruit@gmail.com>
2025-10-06 19:36:20 +0900

    feat: add searchBar in participation screen

--- screens/participation/participation.screen.tsx
+++ screens/participation/participation.screen.tsx
@@ -1,5 +1,9 @@
-import { memo } from 'react';
+import { Input } from '@/components';
+import { DUMMY_ORGANIZATIONS } from '@/dummy';
+import { IOrganizationType } from '@/types';
+import { memo, useCallback, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useDidMount, useDidUpdate } from 'rooks';
 import { Image, ScrollView, Stack, Text } from 'tamagui';
 
 type IOrganizationCardComponentProps = {
@@ -46,23 +50,70 @@ export const ParticipationScreen = memo(() => {
       );
     }
   );
+
+  const [organizations, setOrganizations] = useState<IOrganizationType[]>([]);
+  const [searchedOrganization, setSearchedOrganization] = useState<string>('');
+
+  const handleChangeSearchOrganization = useCallback((text: string) => {
+    setSearchedOrganization(text);
+  }, []);
+
+  useDidMount(() => {
+    //TODO(@Milgam06): 조직 리스트 패칭
+    setOrganizations(DUMMY_ORGANIZATIONS);
+  });
+
+  useDidUpdate(() => {
+    //TODO(@Milgam06): 검색 시, 업데이트된 searchedOrganization 값으로, 패칭한 리스트 내에서 필터링
+    const isSearchEmpty = searchedOrganization.trim() === '';
+    if (isSearchEmpty) {
+      setOrganizations(DUMMY_ORGANIZATIONS);
+      return;
+    }
+    const filteredOrganizations = organizations.filter(({ organizationName }) => {
+      const isOrganizationNameMatch = organizationName.includes(searchedOrganization);
+      return isOrganizationNameMatch;
+    });
+    setOrganizations(filteredOrganizations);
+  }, [searchedOrganization]);
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <Stack flex={1} px="$size.x5" py="$size.x5" gap="$size.x8">
+      <Stack flex={1} px="$size.x5" py="$size.x5" gap="$size.x6">
         <Text fontSize="$9" fontWeight="800">
           내 조직
         </Text>
-        <ScrollView flex={1} width="$fluid">
-          <Stack flex={1} width="$fluid" gap="$size.x2">
-            <OrganizationCardComponent
-              organizationId="1"
-              organizationName="OrganizationTitle"
-              organizationDescription="OrganizationDescription"
-              organizationMemberCount={100}
-              organizationImageUrl="https://picsum.photos/200/300"
-            />
-          </Stack>
-        </ScrollView>
+        <Stack flex={1} width="$fluid" gap="$size.x3">
+          <Input
+            value={searchedOrganization}
+            onChangeText={handleChangeSearchOrganization}
+            w="$fluid"
+            height="$size.x12"
+            placeholder="검색"
+            px="$size.x3"
+            borderColor="$colors.lightGray"
+            fontSize="$5"
+            fontWeight="500"
+            focusStyle={{ borderColor: '$colors.componentGreen' }}
+          />
+          <ScrollView flex={1} width="$fluid">
+            <Stack flex={1} width="$fluid" gap="$size.x2">
+              {organizations.map(({ id, organizationName, organizationDescription, members }) => {
+                const memberCount = members.length;
+                return (
+                  <OrganizationCardComponent
+                    key={id}
+                    organizationId={id}
+                    organizationName={organizationName}
+                    organizationDescription={organizationDescription}
+                    organizationMemberCount={memberCount}
+                    organizationImageUrl="https://picsum.photos/200/300"
+                  />
+                );
+              })}
+            </Stack>
+          </ScrollView>
+        </Stack>
       </Stack>
     </SafeAreaView>
   );
