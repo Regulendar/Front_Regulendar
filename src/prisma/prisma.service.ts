501f3a764dac44996d1413d960e39230265b1339
Milgam06 <milgamfruit@gmail.com>
2025-10-06 17:51:55 +0900

    fix: remove comment

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
-@Injectable() // PrismaService를 DI컨테이너에 등록시킴. 진짜 이해 하나도 안됨 "메이데이"
+@Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
