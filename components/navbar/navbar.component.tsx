f69eaadc94cc91aad0afe7fd12a0a1215ab46fdf
Milgam06 <milgamfruit@gmail.com>
2025-10-13 22:37:44 +0900

    feat: add Navbar in Organization screen

--- components/navbar/navbar.component.tsx
+++ components/navbar/navbar.component.tsx
@@ -1,11 +1,53 @@
-import { memo } from 'react';
-import { Stack, Text } from 'tamagui';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { memo, useCallback } from 'react';
+import { Stack } from 'tamagui';
 
-export const Navbar = memo(() => {
+export type INavbarItem = {
+  value: string;
+  icon: IconDefinition;
+};
+
+type INavbarProps = {
+  itemValue: string;
+  navbarItems: INavbarItem[];
+  onChangeItemValue: (value: string) => void;
+};
+
+export const Navbar = memo<INavbarProps>(({ itemValue, navbarItems, onChangeItemValue }) => {
+  const handlePressItem = useCallback(
+    (value: string) => () => {
+      return onChangeItemValue(value);
+    },
+    [onChangeItemValue]
+  );
   return (
-    <Stack width="$fluid" borderWidth={1} justify="center" items="center" py="$size.x8">
-      <Stack width="$fluid" flexDirection="row" justify="space-between" items="center" borderWidth={1}>
-        <Text>Navbar</Text>
+    <Stack width="$fluid" borderWidth={1} justify="center" items="center" px="$size.x2" py="$size.x10">
+      <Stack
+        width="$fluid"
+        flexDirection="row"
+        justify="space-between"
+        items="center"
+        borderWidth={1}
+        style={{ borderRadius: 12 }}>
+        {navbarItems.map(({ icon, value }, index) => {
+          const isSelected = itemValue === value;
+
+          return (
+            <Stack
+              key={index}
+              width="$fit"
+              borderWidth={1}
+              p="$size.x2"
+              bg={isSelected ? '$colors.componentGreen' : 'transparent'}
+              style={{
+                borderRadius: 48,
+              }}
+              onPress={handlePressItem(value)}>
+              <FontAwesomeIcon size={28} icon={icon} color={isSelected ? '#FFF' : '#000000'} />
+            </Stack>
+          );
+        })}
       </Stack>
     </Stack>
   );
--- screens/organization/organization.screen.tsx
+++ screens/organization/organization.screen.tsx
@@ -1,5 +1,9 @@
-import { Navbar } from '@/components';
-import { memo } from 'react';
+import { INavbarItem, Navbar } from '@/components';
+import { faBell } from '@fortawesome/free-solid-svg-icons/faBell';
+import { faHome } from '@fortawesome/free-solid-svg-icons/faHome';
+import { faMessage } from '@fortawesome/free-solid-svg-icons/faMessage';
+import { faSearch } from '@fortawesome/free-solid-svg-icons/faSearch';
+import { memo, useCallback, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Stack, Text } from 'tamagui';
 
@@ -8,14 +12,27 @@ type IOrganizationScreenProps = {
 };
 
 export const OrganizationScreen = memo<IOrganizationScreenProps>(({ organizationId }) => {
+  const [selectedItem, setSelectedItem] = useState<string>('Home');
+  const navbarItems: INavbarItem[] = [
+    { value: 'Home', icon: faHome },
+    { value: 'Search', icon: faSearch },
+    { value: 'Notifications', icon: faBell },
+    { value: 'Messages', icon: faMessage },
+  ];
+
+  const handleChangeNavbarItem = useCallback((value: string) => {
+    console.log('asfd');
+    setSelectedItem(value);
+  }, []);
+
   return (
     <Stack flex={1}>
-      <SafeAreaView edges={['top']} style={{ flex: 1, borderWidth: 1 }}>
+      <SafeAreaView edges={['top']} style={{ flex: 1 }}>
         <Stack flex={1} width="$fluid">
           <Text>{organizationId} Organization Screen</Text>
         </Stack>
       </SafeAreaView>
-      <Navbar />
+      <Navbar itemValue={selectedItem} navbarItems={navbarItems} onChangeItemValue={handleChangeNavbarItem} />
     </Stack>
   );
 });
