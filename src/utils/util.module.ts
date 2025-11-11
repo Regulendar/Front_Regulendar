4666a7c7208b8d834a16f6faeb2e89287ab92b10
Milgam06 <milgamfruit@gmail.com>
2025-11-11 10:59:10 +0900

    fix: fix DateConverterUtil issue

--- src/utils/util.module.ts
+++ src/utils/util.module.ts
@@ -1,9 +1,10 @@
 import { Global, Module } from '@nestjs/common';
 import { ValidatorUtil } from './validator.util';
+import { DateConverterUtil } from './dateConverter.util';
 
 @Global()
 @Module({
-  providers: [ValidatorUtil],
-  exports: [ValidatorUtil],
+  providers: [ValidatorUtil, DateConverterUtil],
+  exports: [ValidatorUtil, DateConverterUtil],
 })
 export class UtilModule {}
