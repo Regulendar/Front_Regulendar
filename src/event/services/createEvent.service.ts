daadbfc12aeaef5aed8f4642fd6be8d3ff28862c
Milgam06 <milgamfruit@gmail.com>
2025-11-13 16:29:19 +0900

    fix: add event hostUser in participationIds

--- src/event/services/createEvent.service.ts
+++ src/event/services/createEvent.service.ts
@@ -38,7 +38,7 @@ export class CreateEventService {
             },
           },
           hostUserId,
-          participationIds: [],
+          participationIds: [hostUserId],
         },
       });
 
