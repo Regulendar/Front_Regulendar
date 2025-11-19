b2bcaf8032aa3a828e33762c00ee5b58f4cfe917
MILGAM <93873127+Milgam06@users.noreply.github.com>
2025-11-20 00:57:13 +0900

    feat: add event card in organization main subscreen

--- screens/organization/subScreens/main.subscreen.tsx
+++ screens/organization/subScreens/main.subscreen.tsx
@@ -1,5 +1,8 @@
-import { Card } from '@/components';
+import { Button } from '@/components';
 import { getScreenSize } from '@/utils';
+import { faClock } from '@fortawesome/free-solid-svg-icons/faClock';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons/faInfoCircle';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { memo } from 'react';
 import { ScrollView, Stack, Text } from 'tamagui';
 
@@ -8,14 +11,57 @@ export const OrganizationMainSubScreen = memo(() => {
   return (
     <Stack flex={1} width="$fluid" py="$size.x2">
       <ScrollView width="$fluid" horizontal pagingEnabled showsHorizontalScrollIndicator={false} py="$size.x1_5">
-        <Stack width={windowWidth} px="$size.x5">
-          <Card width="$fluid" type="custom" backgroundColor="$colors.componentGreen">
-            <Stack width="$fluid">
-              <Text>
-                This is the main sub-screen for the organization section. Here you can find various details and
-              </Text>
+        <Stack width={windowWidth} px="$size.x3">
+          <Stack
+            width="$fluid"
+            justify="space-between"
+            py="$size.x3"
+            bg="$colors.componentGreen"
+            style={{ borderRadius: 12 }}>
+            <Stack
+              flexDirection="row"
+              justify="space-between"
+              items="center"
+              px="$size.x4"
+              pb="$size.x3"
+              borderBottomWidth={1}
+              borderBottomColor="$colors.backgroundWhite"
+              borderStyle="dashed">
+              <Stack gap="$size.x1">
+                <Text fontSize="$9" fontWeight="900" color="$colors.backgroundWhite">
+                  Event name
+                </Text>
+                <Stack flexDirection="row" gap="$size.x1_5" items="center">
+                  <FontAwesomeIcon icon={faClock} color="#f5f5f5" />
+                  <Text fontSize="$6" fontWeight="900" color="$colors.backgroundWhite">
+                    Event duration
+                  </Text>
+                </Stack>
+              </Stack>
+              <Stack
+                px="$size.x2"
+                py="$size.x1"
+                bg="$colors.backgroundWhite"
+                justify="center"
+                items="center"
+                style={{ borderRadius: 8 }}>
+                <Text fontSize="$8" fontWeight="900" color="$colors.componentGreen">
+                  12
+                </Text>
+                <Text fontSize="$8" fontWeight="900" color="$colors.componentGreen">
+                  Wed
+                </Text>
+              </Stack>
             </Stack>
-          </Card>
+            <Stack width="$fluid" px="$size.x4" pt="$size.x3" items="flex-start">
+              <Button isFullWidth={false} px="$size.x3" py="$size.x1_5" backgroundColor="$colors.backgroundWhite">
+                <FontAwesomeIcon size={20} icon={faInfoCircle} color="#3ABF67" />
+                <Text fontSize="$6" fontWeight="700" color="$colors.componentGreen">
+                  Details
+                </Text>
+              </Button>
+            </Stack>
+          </Stack>
         </Stack>
       </ScrollView>
     </Stack>
