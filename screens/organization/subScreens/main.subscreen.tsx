8a9e101bb8edb5522f0e0469203be632b36f2fb8
MILGAM <93873127+Milgam06@users.noreply.github.com>
2025-11-25 17:16:10 +0900

    fix: update minor style

--- screens/organization/subScreens/main.subscreen.tsx
+++ screens/organization/subScreens/main.subscreen.tsx
@@ -89,73 +89,213 @@ export const OrganizationMainSubScreen = memo(() => {
   );
 
   return (
-    <Stack flex={1} width="$fluid" py="$size.x2" gap="$size.x5">
-      <Stack gap="$size.x2">
-        <Stack px="$size.x5" flexDirection="row" gap="$size.x2" items="flex-end">
-          <Text fontSize="$6" fontWeight="900">
-            이번 달 참여 이벤트
-          </Text>
-          <Text fontSize="$5" fontWeight="900" color="$colors.mediumGray">
-            0 / 2
-          </Text>
-        </Stack>
-        <ScrollView width="$fluid" horizontal pagingEnabled showsHorizontalScrollIndicator={false}>
-          <ParticipatedEventCardComponent
-            eventDateDay={18}
-            eventDateMonth={9}
-            eventDateYear={2025}
-            eventDuration={30}
-            eventId="1"
-            eventTitle="프로젝트 마감"
-          />
-        </ScrollView>
-      </Stack>
-      <Stack gap="$size.x2">
-        <Stack px="$size.x5">
-          <Text fontSize="$6" fontWeight="900">
-            활동
-          </Text>
-        </Stack>
-        <Stack width="$fluid" flexDirection="row" justify="space-between" px="$size.x3" gap="$size.x2">
-          <Stack
-            flex={1}
-            aspectRatio={1}
-            justify="center"
-            items="center"
-            py="$size.x3"
-            bg="$colors.componentGreen"
-            gap="$size.x0_5"
-            style={{ borderRadius: 12 }}>
-            <Text fontSize="$13" fontWeight="900" color="$colors.backgroundWhite">
-              {value}%
+    <ScrollView flex={1} width="$fluid">
+      <Stack flex={1} width="$fluid" py="$size.x2" gap="$size.x5">
+        <Stack gap="$size.x2">
+          <Stack px="$size.x5" flexDirection="row" gap="$size.x2" items="flex-end">
+            <Text fontSize="$6" fontWeight="900">
+              이번 달 참여 이벤트
             </Text>
-            <Text fontSize="$6" fontWeight="900" color="$colors.backgroundWhite">
-              이번 달 참여율
+            <Text fontSize="$5" fontWeight="900" color="$colors.mediumGray">
+              0 / 2
             </Text>
           </Stack>
-          <Stack
-            flex={1}
-            aspectRatio={1}
-            justify="center"
-            items="center"
-            py="$size.x3"
-            bg="$colors.backgroundWhite"
-            gap="$size.x1"
-            borderWidth={1}
-            borderColor="$colors.componentGreen"
-            style={{ borderRadius: 12 }}>
-            <Image
-              source="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Thinking%20Face.png"
-              alt="Thinking Face"
-              contentFit="contain"
-              style={{ width: 100, aspectRatio: 1 }}
+          <ScrollView width="$fluid" horizontal pagingEnabled showsHorizontalScrollIndicator={false}>
+            <ParticipatedEventCardComponent
+              eventDateDay={18}
+              eventDateMonth={9}
+              eventDateYear={2025}
+              eventDuration={30}
+              eventId="1"
+              eventTitle="프로젝트 마감"
             />
-            <Text fontSize="$5" fontWeight="900" color="$colors.componentGreen">
-              아직 게시물이 없어요!
+          </ScrollView>
+        </Stack>
+        <Stack gap="$size.x2">
+          <Stack px="$size.x5">
+            <Text fontSize="$6" fontWeight="900">
+              활동
             </Text>
+          </Stack>
+          <Stack width="$fluid" flexDirection="row" justify="space-between" px="$size.x3" gap="$size.x2">
+            <Stack
+              flex={1}
+              aspectRatio={1}
+              justify="center"
+              items="center"
+              py="$size.x3"
+              bg="$colors.componentGreen"
+              gap="$size.x0_5"
+              style={{ borderRadius: 12 }}>
+              <Text fontSize="$13" fontWeight="900" color="$colors.backgroundWhite">
+                {value}%
+              </Text>
+              <Text fontSize="$6" fontWeight="900" color="$colors.backgroundWhite">
+                이번 달 참여율
+              </Text>
+            </Stack>
+            <Stack
+              flex={1}
+              aspectRatio={1}
+              justify="center"
+              items="center"
+              py="$size.x3"
+              bg="$colors.backgroundWhite"
+              gap="$size.x1"
+              borderWidth={1}
+              borderColor="$colors.componentGreen"
+              style={{ borderRadius: 12 }}>
+              <Image
+                source="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Thinking%20Face.png"
+                alt="Thinking Face"
+                contentFit="contain"
+                style={{ width: 100, aspectRatio: 1 }}
+              />
+              <Text fontSize="$5" fontWeight="900" color="$colors.componentGreen">
+                아직 게시물이 없어요!
+              </Text>
+            </Stack>
+          </Stack>
+        </Stack>
+        <Stack gap="$size.x2">
+          <Stack px="$size.x5">
+            <Text fontSize="$6" fontWeight="900">
+              이런 이벤트는 어때요?
+            </Text>
+          </Stack>
+
+          <Stack px="$size.x3" gap="$size.x2">
+            <Stack
+              width="$fluid"
+              p="$size.x2"
+              flexDirection="row"
+              borderWidth="$size.x0_5"
+              borderColor="$colors.darkGreen"
+              bg="$colors.backgroundWhite"
+              gap="$size.x2"
+              style={{ borderRadius: 8 }}>
+              <Stack
+                px="$size.x2"
+                py="$size.x1"
+                bg="$colors.componentGreen"
+                justify="center"
+                items="center"
+                style={{ borderRadius: 4 }}>
+                <Text fontSize="$8" fontWeight="900" color="$colors.backgroundWhite">
+                  12
+                </Text>
+                <Text fontSize="$8" fontWeight="900" color="$colors.backgroundWhite">
+                  23
+                </Text>
+              </Stack>
+              <Stack>
+                <Text fontSize="$8" fontWeight="900" color="$colors.backgroundBlack">
+                  eventTitle
+                </Text>
+                <Text fontSize="$4" fontWeight="600" color="$colors.backgroundBlack">
+                  소요시간: 60
+                </Text>
+              </Stack>
+            </Stack>
+            <Stack
+              width="$fluid"
+              p="$size.x2"
+              flexDirection="row"
+              borderWidth="$size.x0_5"
+              borderColor="$colors.darkGreen"
+              bg="$colors.backgroundWhite"
+              gap="$size.x2"
+              style={{ borderRadius: 8 }}>
+              <Stack
+                px="$size.x2"
+                py="$size.x1"
+                bg="$colors.componentGreen"
+                justify="center"
+                items="center"
+                style={{ borderRadius: 4 }}>
+                <Text fontSize="$8" fontWeight="900" color="$colors.backgroundWhite">
+                  12
+                </Text>
+                <Text fontSize="$8" fontWeight="900" color="$colors.backgroundWhite">
+                  23
+                </Text>
+              </Stack>
+              <Stack>
+                <Text fontSize="$8" fontWeight="900" color="$colors.backgroundBlack">
+                  eventTitle
+                </Text>
+                <Text fontSize="$4" fontWeight="600" color="$colors.backgroundBlack">
+                  소요시간: 60
+                </Text>
+              </Stack>
+            </Stack>
+            <Stack
+              width="$fluid"
+              p="$size.x2"
+              flexDirection="row"
+              borderWidth="$size.x0_5"
+              borderColor="$colors.darkGreen"
+              bg="$colors.backgroundWhite"
+              gap="$size.x2"
+              style={{ borderRadius: 8 }}>
+              <Stack
+                px="$size.x2"
+                py="$size.x1"
+                bg="$colors.componentGreen"
+                justify="center"
+                items="center"
+                style={{ borderRadius: 4 }}>
+                <Text fontSize="$8" fontWeight="900" color="$colors.backgroundWhite">
+                  12
+                </Text>
+                <Text fontSize="$8" fontWeight="900" color="$colors.backgroundWhite">
+                  23
+                </Text>
+              </Stack>
+              <Stack>
+                <Text fontSize="$8" fontWeight="900" color="$colors.backgroundBlack">
+                  eventTitle
+                </Text>
+                <Text fontSize="$4" fontWeight="600" color="$colors.backgroundBlack">
+                  소요시간: 60
+                </Text>
+              </Stack>
+            </Stack>
+            <Stack
+              width="$fluid"
+              p="$size.x2"
+              flexDirection="row"
+              borderWidth="$size.x0_5"
+              borderColor="$colors.darkGreen"
+              bg="$colors.backgroundWhite"
+              gap="$size.x2"
+              style={{ borderRadius: 8 }}>
+              <Stack
+                px="$size.x2"
+                py="$size.x1"
+                bg="$colors.componentGreen"
+                justify="center"
+                items="center"
+                style={{ borderRadius: 4 }}>
+                <Text fontSize="$8" fontWeight="900" color="$colors.backgroundWhite">
+                  12
+                </Text>
+                <Text fontSize="$8" fontWeight="900" color="$colors.backgroundWhite">
+                  23
+                </Text>
+              </Stack>
+              <Stack>
+                <Text fontSize="$8" fontWeight="900" color="$colors.backgroundBlack">
+                  eventTitle
+                </Text>
+                <Text fontSize="$4" fontWeight="600" color="$colors.backgroundBlack">
+                  소요시간: 60
+                </Text>
+              </Stack>
+            </Stack>
           </Stack>
         </Stack>
       </Stack>
-    </Stack>
+    </ScrollView>
   );
 });
