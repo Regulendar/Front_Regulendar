33ac4d009ada6826cbfd9389c557138a62c6d469
MILGAM <93873127+Milgam06@users.noreply.github.com>
2025-12-09 12:34:41 +0900

    feat(back): add ConfigModule to use env config

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,21 +1,26 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'node:path';
 import { UserModule } from './user';
 import { PrismaModule } from './prisma';
 import { EventModule } from './event';
 import { UtilModule } from './utils';
 import { OrganizationModule } from './organization';
-import { join } from 'node:path';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(
         process.cwd(),
         '../../packages/shared/graphql/schema.gql',
       ),
+      introspection: false,
       graphiql: true,
     }),
     PrismaModule,
