a6143b305af77fa61457fbc2e8dedb5c987a6215
MILGAM <93873127+Milgam06@users.noreply.github.com>
2025-12-09 22:46:46 +0900

    fix(front): fix GRAPHQL_URL in apollo.lib.ts to prevent 'Apollo Network request failed'

--- apps/client/libs/apollo/apolloClient.lib.ts
+++ apps/client/libs/apollo/apolloClient.lib.ts
@@ -1,6 +1,15 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import { Platform } from 'react-native';
 
-const GRAPHQL_URL = process.env.EXPO_PUBLIC_GRAPHQL_URL ?? 'http://localhost:3000/graphql';
+const getGraphQLUrl = () => {
+  if (process.env.EXPO_PUBLIC_GRAPHQL_URL) {
+    return process.env.EXPO_PUBLIC_GRAPHQL_URL;
+  }
+  const host = Platform.OS === 'android' ? '10.0.2.2' : 'localhost';
+  return `http://${host}:3000/graphql`;
+};
+
+const GRAPHQL_URL = getGraphQLUrl();
 
 export const apolloClient = new ApolloClient({
   link: new HttpLink({
