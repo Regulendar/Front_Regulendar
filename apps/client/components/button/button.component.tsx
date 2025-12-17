c326d6524a655d1ed990de2328f0c56ac67c2ab0
Milgam06 <milgamfruit@gmail.com>
2025-12-17 14:21:38 +0900

    fix(front): add style props in Button component

--- apps/client/components/button/button.component.tsx
+++ apps/client/components/button/button.component.tsx
@@ -1,17 +1,18 @@
 import { memo, ReactNode } from 'react';
-import { FlexAlignType, OpaqueColorValue } from 'react-native';
+import { FlexAlignType, OpaqueColorValue, StyleProp, ViewStyle } from 'react-native';
 import { GetThemeValueForKey, Button as TamaguiButton } from 'tamagui';
 
 type IJustify = 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around' | 'space-evenly';
 
 type IButton = {
   isFullWidth?: boolean;
-  backgroundColor?: OpaqueColorValue | GetThemeValueForKey<'backgroundColor'>;
+  bg?: OpaqueColorValue | GetThemeValueForKey<'backgroundColor'>;
   justify?: IJustify;
   items?: FlexAlignType;
   px?: GetThemeValueForKey<'paddingHorizontal'>;
   py?: GetThemeValueForKey<'paddingVertical'>;
   disableShadow?: boolean;
+  style?: StyleProp<ViewStyle>;
   children: ReactNode;
   onPressButton?: () => void;
 };
@@ -19,12 +20,13 @@ type IButton = {
 export const Button = memo<IButton>(
   ({
     isFullWidth = true,
-    backgroundColor = '$colors.componentGreen',
+    bg = '$colors.componentGreen',
     justify = 'center',
     items = 'center',
     px = '$size.x6',
     py = '$size.x3',
     disableShadow = false,
+    style,
     children,
     onPressButton,
   }) => {
@@ -36,9 +38,10 @@ export const Button = memo<IButton>(
         items={items}
         px={px}
         py={py}
-        bg={backgroundColor}
+        bg={bg}
         boxShadow={disableShadow ? '' : '0 2px 20px rgba(0, 0, 0, 0.2)'}
         pressStyle={{ opacity: 0.8 }}
+        style={style}
         onPress={onPressButton}>
         {children}
       </TamaguiButton>
